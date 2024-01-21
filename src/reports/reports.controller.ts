74cb812c97704b18ecbc44d4e1b4375370408907
juan <bustamante.jpm@gmail.com>
2024-01-21 17:16:47 -0300

    - e2e test fix - report typeORM relations

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Patch, Param } from '@nestjs/common';
+import { CreateReportDTO } from './dtos/create-report-dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/gurds/auth.gurds';
+import { CurrentUser } from 'src/users/decorators/currentUser.decorator';
+import  { User } from '../users/user.entity'
+import { ReportDTO } from './dtos/report.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report-dto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private reportsService: ReportsService) {}
+
+    @Post()
+    @UseGuards(AuthGuard)
+    @Serialize(ReportDTO)
+    createReport(@Body() body: CreateReportDTO, @CurrentUser() user:User) {
+        return this.reportsService.create(body, user);
+    }
+
+    @Patch('/:id')
+    approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+
+    }
+}
